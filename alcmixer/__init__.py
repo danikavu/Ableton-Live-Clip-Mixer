1819c2912283b6613df5fa91f2e40ecb4a3a6c80
unknown <dan_kavoulakos@hotmail.com>
2022-06-13 13:30:39 +0300

    - Update __init__.py
    Json directory func added

--- alcmixer/__init__.py
+++ alcmixer/__init__.py
@@ -1,9 +1,10 @@
 # -*- pretty cool init mate? -*-
 from alcmixer.generate_midi import generate_pattern
-from alcmixer.extract_alc_features import extract_alc_features, alc_path
+from alcmixer.extract_alc_features import extract_alc_features, alc_path, make_json_dir
 
 __all__ = [
     'generate_pattern',
     'extract_alc_features',
     'alc_path',
+    'make_json_dir',
     ]
